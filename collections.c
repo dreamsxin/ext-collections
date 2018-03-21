012dc887fb100f47a3075e6af2ad7239472f5e06
CismonX <qiaocheng@twt.edu.cn>
2018-03-21 16:26:14 +0800

    indent with spaces instead of tabs

--- collections.c
+++ collections.c
@@ -20,35 +20,35 @@ PHP_MINIT_FUNCTION(collections)
     zend_register_internal_class(&collections_collection_ce);
     INIT_CLASS_ENTRY_EX(collections_pair_ce, "Pair", strlen("Pair"), pair_methods);
     zend_register_internal_class(&collections_pair_ce);
-	return SUCCESS;
+    return SUCCESS;
 }
 
 PHP_RINIT_FUNCTION(collections)
 {
 #if defined(COMPILE_DL_COLLECTIONS) && defined(ZTS)
-	ZEND_TSRMLS_CACHE_UPDATE();
+    ZEND_TSRMLS_CACHE_UPDATE();
 #endif
-	return SUCCESS;
+    return SUCCESS;
 }
 
 PHP_MINFO_FUNCTION(collections)
 {
-	php_info_print_table_start();
-	php_info_print_table_header(2, "collections support", "enabled");
-	php_info_print_table_end();
+    php_info_print_table_start();
+    php_info_print_table_header(2, "collections support", "enabled");
+    php_info_print_table_end();
 }
 
 zend_module_entry collections_module_entry = {
-	STANDARD_MODULE_HEADER,
-	"collections",
-	NULL,
-	PHP_MINIT(collections),
-	NULL,
-	PHP_RINIT(collections),
-	NULL,
-	PHP_MINFO(collections),
-	PHP_COLLECTIONS_VERSION,
-	STANDARD_MODULE_PROPERTIES
+    STANDARD_MODULE_HEADER,
+    "collections",
+    NULL,
+    PHP_MINIT(collections),
+    NULL,
+    PHP_RINIT(collections),
+    NULL,
+    PHP_MINFO(collections),
+    PHP_COLLECTIONS_VERSION,
+    STANDARD_MODULE_PROPERTIES
 };
 
 #ifdef COMPILE_DL_COLLECTIONS
